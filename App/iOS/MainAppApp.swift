3f7c9f5c1544fc2f6593e7136a801bb4898ff869
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-16 07:58:34 +0000

    added code to test supporter library

--- App/iOS/MainAppApp.swift
+++ App/iOS/MainAppApp.swift
@@ -3,13 +3,30 @@ import SwiftUI
 
 @main
 struct MainAppApp: App {
-    let viewModel: Kit.SettingsViewModel = .init(route: .profile)
-    
+    //let viewModel: Kit.SettingsViewModel = .init(route: .profile)
+    //@State var openSupport = false
+  
     var body: some Scene {
         WindowGroup {
             Kit.HomeScreen(viewModel: .init())
+
         }
     }
 }
 
-
+#warning("code to test Supporter library")
+//            NavigationView {
+//                Kit.HomeScreen(viewModel: .init())
+//                    .toolbar {
+//                        ToolbarItem {
+//                            Button {
+//                                openSupport.toggle()
+//                            } label: {
+//                                Image(systemName: "questionmark.circle.fill")
+//                            }
+//                        }
+//                    }
+//                    .fullScreenCover(isPresented: $openSupport) {
+//                        SupportScreen()
+//                    }
+//            }
