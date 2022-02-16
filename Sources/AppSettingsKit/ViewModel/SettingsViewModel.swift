4c239d2fdea30052c5f80a0894291d816d2c5a9a
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-16 10:18:10 +0000

    added bindable objects for `.toggle` `ViewType`

--- Sources/AppSettingsKit/ViewModel/SettingsViewModel.swift
+++ Sources/AppSettingsKit/ViewModel/SettingsViewModel.swift
@@ -3,8 +3,15 @@ import SwiftUI
 
 extension Kit {
     public class SettingsViewModel: ObservableObject {
-        @Published public var route = Kit.Route.home 
-        @Published public var isActive = false 
+        @Published public var route = Kit.Route.home
+        
+        #warning("we cannot have more than 10 of this")
+        //Possible solution
+        //Register a name of saving the state of each Bool setting
+        //Dynamically generate a bindable object which each encounter
+        @Published public var isActive = false
+        @Published public var isAirplaneMode = false
+        @Published public var isVPN = false 
         
         public init() {}
         public init(
