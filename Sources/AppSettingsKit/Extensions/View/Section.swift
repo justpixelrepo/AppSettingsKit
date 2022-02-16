5978d5725e1c4c154f6f788fc32373171621d6a3
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-02-16 10:19:10 +0000

    possible Section extension for taking control back

---
+++ Sources/AppSettingsKit/Extensions/View/Section.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+//public extension Section where
+//    Footer == VStack<TupleView<(Text,Text)>>,
+//{
+//    init(
+//        profile: Kit.Setting.Profile
+//    ) {
+//        self.init {
+//
+//        }
+//    }
+//}
