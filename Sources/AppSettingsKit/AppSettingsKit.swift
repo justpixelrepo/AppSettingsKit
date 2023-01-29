db9a1f75c1f2d4030a56887592bc6636bdf67f7e
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2023-01-29 01:36:52 +0000

    added family view and models

--- Sources/AppSettingsKit/AppSettingsKit.swift
+++ Sources/AppSettingsKit/AppSettingsKit.swift
@@ -1,3 +1,6 @@
 
 public typealias Kit = AppSettingsKit
+public typealias Settings = Kit.Setting
+public typealias Profile = Kit.Setting.Profile
+public typealias Family = Kit.Setting.Profile.Family
 public struct AppSettingsKit {}
